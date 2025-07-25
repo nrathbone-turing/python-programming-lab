8908d936efab64fe024ba2793dc932f615e86223
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-25 16:21:10 -0600

    Upload debugged python file for pizza order cost and updated readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# Scenario
+You work for a pizza delivery company that needs a program to calculate order costs. The company has specific pricing rules:
+
+## Details:
+Pizza sizes and base prices:
+- Small pizza: $8
+- Large pizza: $12
+
+Toppings: $1 for each additional topping
+
+Delivery fee:
+- $2 for the first 5 miles
+- $1 for each additional mile
+
+## Your task
+Create a Python script that prompts the user for order details, calculates the total cost, and displays the result.
---
+++ pizza_order_cost.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+'''
+This script takes in three inputs
+- pizza size : (small / large) string
+- number of toppings : integer
+- delivery distance (in miles) :integer
+'''
+
+# pizza size input and validation loop
+while True:
+    pizza_size = str(input("Which size pizza would you like (small / large)?:").lower())
+    # small pizza = $8; large pizza = $12
+    if pizza_size == "small":
+        pizza_cost = int(8)
+        break
+    elif pizza_size == "large":
+        pizza_cost = int(12)
+        break
+    # break loop & return to let the user retry input if invalid
+    else:
+        print("Please choose either 'small' or 'large'")
+
+# toppings count input
+toppings_count = int(input("How many toppings would you like?:"))
+# toppings = $1 per topping
+toppings_cost = 1 * toppings_count
+
+# delivery distance input
+delivery_distance = int(input("How far away is the destination (in miles)?:"))
+# $2 for first 5 miles; $1 for each additional mile
+if delivery_distance == 0:
+    delivery_fee = int(0)
+elif delivery_distance <= 5:
+    delivery_fee = int(2)
+elif delivery_distance:
+    # base cost of 2 for 5 or more miles + (total distance - 5) to account for base cost 
+    # and add the rest at the additional mile rate
+    delivery_fee = int(2 + (delivery_distance - 5))
+
+# total cost
+total_cost = pizza_cost + toppings_cost + delivery_fee
+
+# itemized receipt
+print(f"\nYour order summary:")
+print(f"- One {pizza_size} pizza: ${pizza_cost}")
+print(f"- {toppings_count} topping(s): ${toppings_cost}")
+print(f"- Delivery fee ({delivery_distance} miles): ${delivery_fee}")
+print(f"TOTAL COST: ${total_cost}")
+
+# sentence receipt
+print()
+print()
+print(f"The total cost for your order for a {pizza_size} pizza with {toppings_count} toppings and a delivery distance of {delivery_distance} miles comes out to ${total_cost}.")
